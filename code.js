f04e6b26dba4748fff57bdac6efbb0a00b0d440c
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-17 16:22:13 -0500

    s

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 var s = document.getElementsByClassName('infoTextEmail');
 function login() {
-    setTimeout(() => {  alert(s)}, 1000);
+    for(var i=0;i < s.length; i++){
+        console.log(s[i].textContent);
+     }
 }
 
 netlifyIdentity.on('login', user => login());
