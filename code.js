04d460a534c3899292dfcc915aaa56a413b44dfc
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-17 13:08:42 -0500

    s

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 var s = netlifyIdentity.currentUser.user
 function login() {
-    document.write('netlifyIdentity.currentUser()')
+    document.write(netlifyIdentity.currentUser.user())
 }
 
 netlifyIdentity.on('login', user => login());
